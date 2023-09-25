2ab2425b4d48686d22232d4ffc1916cf71f945d4
Jeroni Brunet <jeroni@gmail.com>
2023-09-25 08:45:38 +0200

    do more porting - WIP

---
+++ quest-client/src/app/components/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  template: `<p>Quest Dashboard</p>`
+})
+export default class DashboardComponent {}
